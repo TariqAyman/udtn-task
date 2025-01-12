efefef3e9e9b8dcfdcb53818cd77af710e6dd4c0
Tariq Ayman <tariq.ayman94@gmail.com>
2025-01-13 01:49:29 +0200

    fix: show request Validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { useContainer, ValidationError } from 'class-validator';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  // app use global pipes to automatically validate requests
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true, // Enables automatic data transformation
+      whitelist: true, // Strips properties without decorators
+      forbidNonWhitelisted: true, // Throws an error if non-whitelisted properties are present
+      transformOptions: { enableImplicitConversion: true },
+    }),
+  );
+
+  useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
   // Swagger setup
   const config = new DocumentBuilder()
@@ -14,6 +29,12 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-doc', app, document);
+
+  app.enableCors({
+    origin: true, // This allows all domains
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 
