0c98347d3ca231039f98ac74b9bb6b78fa03cf10
Tariq Ayman <tariq.ayman94@gmail.com>
2025-01-13 01:15:23 +0200

    add Swagger endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Swagger setup
+  const config = new DocumentBuilder()
+    .setTitle('Auth & Products API')
+    .setDescription('API for user authentication and product management')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-doc', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log('Application is running on: http://localhost:3000');
 }
 bootstrap();
