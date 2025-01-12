b9f4aaf005b5b73046ac707fee763d2063c4e7b3
Tariq Ayman <tariq.ayman94@gmail.com>
2025-01-13 01:15:08 +0200

    feat: import modules and config TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { MiddlewareConsumer, Module, RequestMethod } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
+import databaseConfig from './database/typeorm.config';
+import { AppLoggerMiddleware } from './common/middleware/app-log.middleware';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [databaseConfig] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        ...configService.getOrThrow('databaseConfig'),
+      }),
+    }),
+    AuthModule,
+    ProductsModule,
+  ],
+  controllers: [],
+  providers: [],
+  exports: [TypeOrmModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(AppLoggerMiddleware)
+      .forRoutes({ path: '*', method: RequestMethod.ALL }); // Apply globally
+  }
+}
